fef804c90ebea8378e17f091177c6f7bed4fb986
huyhoangy <huyhoangwf@gmail.com>
2025-10-08 08:58:07 +0700

    commit search in route

--- src/route/search.js
+++ src/route/search.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const Router = express.Router();
-const searchController = require('..//app/controlers/SearchController');
+const searchController = require('..//app/controlers/searchController');
 Router.get('/', searchController.find);
 module.exports = Router;
